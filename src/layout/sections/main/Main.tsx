690cc3a3fbb4ff6996e9893f0ff4bb4654e5192a
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-08 16:30:07 +0300

    fixed component Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import photo from './../../../assets/images/photo.jpg';
+import { FlexWrapper } from '../../../components/FlexWrapper';
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi There</span>
-                <h2>I am Svetlana Dyablo</h2>
-                <h1>A Web Developer.</h1>
-            </div>
-            <Photo src={photo} alt="photo" />
-        </div>
+        <StyledMain>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+                <div>
+                    <WelcomeText>Hi There</WelcomeText>
+                    <Name>I am Svetlana Dyablo</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+                <Photo src={photo} alt="photo" />
+            </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #1f1f20;
+`;
+
+const MainTitle = styled.h1``;
+
+const Name = styled.h2``;
+
+const WelcomeText = styled.span``;
 
 const Photo = styled.img`
     width: 350px;
