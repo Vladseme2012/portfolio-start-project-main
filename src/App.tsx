98a6247f7a0c37a051beee12cfd680fafb706c19
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-08 21:41:15 +0300

    import component Works in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import styled from 'styled-components';
 import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
+import { Works } from './layout/sections/works/Works';
 
 function App() {
     return (
@@ -10,14 +10,9 @@ function App() {
             <Header />
             <Main />
             <Skills />
+            <Works />
         </div>
     );
 }
 
 export default App;
-
-// const Title = styled.h1`
-//     font-size: 1.5em;
-//     text-align: center;
-//     color: #e91e63;
-// `;
