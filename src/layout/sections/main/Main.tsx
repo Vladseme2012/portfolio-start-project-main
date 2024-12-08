6aaa8ab2a9fa51e5736e5b2e0fb872d04c32a25c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-08 15:38:04 +0300

    added component Main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <span>Hi There</span>
+                <h2>I am Svetlana Dyablo</h2>
+                <h1>A Web Developer.</h1>
+            </div>
+            <img src="./../../assets/images/phtoto.jpg" alt="" />
+        </div>
+    );
+};
