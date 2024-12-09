226c759c72432432929e8f0362f3691db812e23e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-09 20:37:33 +0300

    added import Slider

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Icon } from '../../../components/icon/Icon';
+import { Slider } from '../../../components/slider/Slider';
+
 
 export const Testimony = () => {
     return (
