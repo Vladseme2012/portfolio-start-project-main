c3e92ce10d5706b08143e5cccca4230de820ca69
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-09 21:01:23 +0300

    fixed styles in Slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FlexWrapper } from '../FlexWrapper';
 
 export const Slider = () => {
     return (
         <StyledSlider>
-            <Slide>
-                <Text>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit.
-                </Text>
-                <Name>@ivan ivanow</Name>
-            </Slide>
+            <FlexWrapper>
+                <Slide>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit.
+                    </Text>
+                    <Name>@ivan ivanow</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Pagination>
         </StyledSlider>
     );
 };
 
 const StyledSlider = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     max-width: 500px;
     border: 1px solid red;
 `;
@@ -29,3 +40,13 @@ const Slide = styled.div`
 const Text = styled.p``;
 
 const Name = styled.span``;
+
+const Pagination = styled.div`
+    span {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        margin: 5px;
+        background-color: black;
+    }
+`;
