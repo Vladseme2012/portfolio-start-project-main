35de318310ad49176d31e5f6ad151d3ad1914ee4
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-12 21:26:07 +0300

    fixed styles for  Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
 import { Menu } from '../../components/menu/Menu';
 import { Container } from '../../components/Container';
+import { FlexWrapper } from '../../components/FlexWrapper';
+import { theme } from '../../styles/Theme';
 
 const itemsMenu = ['Home', 'Skills', 'Works', 'Testimony', 'Contact'];
 
@@ -10,15 +12,21 @@ export const Header = () => {
     return (
         <StyledHeader>
             <Container>
-                <Logo />
-                <Menu items={itemsMenu} />
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo />
+                    <Menu items={itemsMenu} />
+                </FlexWrapper>
             </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: #1f1f20;
-    display: flex;
-    justify-content: space-between;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: ${theme.colors.primaryBg};
+    padding: 20px 0;
+    z-index: 100;
 `;
