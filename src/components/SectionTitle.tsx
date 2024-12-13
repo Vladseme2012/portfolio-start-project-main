7419255b377514226ea6579868127c08065b18ef
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-13 22:39:07 +0300

    fixed styles for SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
+import { theme } from '../styles/Theme';
 
 export const SectionTitle = styled.h2`
-    color: red;
+    position: relative;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 600;
+    font-style: 36px;
+    letter-spacing: 5px;
     text-align: center;
+    margin-bottom: 90px;
+
+    &::before {
+        display: inline-block;
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        transform: translateX(-50%);
+        content: '';
+        width: 55px;
+        height: 1px;
+        background-color: ${theme.colors.accent};
+    }
 `;
