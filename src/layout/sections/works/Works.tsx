452c7a2194f9b203de69075e62d62cea6567f565
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-14 13:42:11 +0300

    fixed component Works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import socialNetwork from './../../../assets/images/social-network.png';
 import counter from './../../../assets/images/counter.png';
+import { Container } from '../../../components/Container';
 import { SectionTitle } from '../../../components/SectionTitle';
-import { Menu } from '../../../components/menu/Menu';
+import { TabMenu } from './tabMenu/TabMenu';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Work } from './work/Work';
 
@@ -12,29 +13,28 @@ const itemsWorks = ['All', 'landing page', 'React', 'spa'];
 export const Works = () => {
     return (
         <StyledSectionWorks>
-            <SectionTitle>My Works</SectionTitle>
-            <Menu items={itemsWorks} />
-            <FlexWrapper justify={'space-around'}>
-                <Work
-                    src={socialNetwork}
-                    title={'Social Network'}
-                    text={
-                        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-                    }
-                />
-                <Work
-                    src={counter}
-                    title={'Timer'}
-                    text={
-                        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim'
-                    }
-                />
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My Works</SectionTitle>
+                <TabMenu items={itemsWorks} />
+                <FlexWrapper justify={'space-between'}>
+                    <Work
+                        src={socialNetwork}
+                        title={'Social Network'}
+                        text={
+                            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                        }
+                    />
+                    <Work
+                        src={counter}
+                        title={'Timer'}
+                        text={
+                            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore magna aliqua Ut enim'
+                        }
+                    />
+                </FlexWrapper>
+            </Container>
         </StyledSectionWorks>
     );
 };
 
-const StyledSectionWorks = styled.section`
-    min-height: 100vh;
-    background-color: #1f1f20;
-`;
+const StyledSectionWorks = styled.section``;
