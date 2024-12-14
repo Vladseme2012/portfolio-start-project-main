e9093dd733f17b7ec366faede01fbaa183ca6342
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-14 19:52:09 +0300

    fixed component Slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Container } from '../../../components/Container';
+import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/Button';
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
-            <Button>Hire me</Button>
+            <Container>
+                <FlexWrapper direction={'column'} align={'center'}>
+                    <SectionTitle>I Am Available For Freelance</SectionTitle>
+                    <Button>Hire me</Button>
+                </FlexWrapper>
+            </Container>
         </StyledSlogan>
     );
 };
