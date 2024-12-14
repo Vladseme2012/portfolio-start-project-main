2915bc2f2ba411435e107d72c494312c26f3bce3
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-14 10:02:20 +0300

    fixed structure and styles in component Skill

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FlexWrapper } from '../../../../components/FlexWrapper';
 import { Icon } from '../../../../components/icon/Icon';
 
 type SkillPropsType = {
@@ -11,16 +12,49 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <SkillArticle>
-            <Icon iconId={props.iconId} />
-            <SkillTitle>{props.title}</SkillTitle>
-            <SkillDescription>{props.text}</SkillDescription>
+            <FlexWrapper direction={'column'} align={'center'}>
+                <IconWrapper>
+                    <Icon iconId={props.iconId} />
+                </IconWrapper>
+                <SkillTitle>{props.title}</SkillTitle>
+                <SkillDescription>{props.text}</SkillDescription>
+            </FlexWrapper>
         </SkillArticle>
     );
 };
 
-const SkillArticle = styled.div`
-    background-color: #1f1f20;
-    min-height: 100vh;
+const SkillArticle = styled.article`
+    max-width: 380px;
+    padding: 62px 20px 40px;
 `;
-const SkillTitle = styled.h3``;
-const SkillDescription = styled.p``;
+
+const IconWrapper = styled.div`
+    position: relative;
+    &::before {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform-origin: top left;
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+        background-color: rgba(255, 255, 255, 0.1);
+        transform: rotate(45deg) translate(-50%, -50%);
+        content: '';
+    }
+`;
+
+const SkillTitle = styled.h3`
+    margin: 70px 0 15px;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-style: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+`;
+
+const SkillDescription = styled.p`
+    text-align: center;
+    font-weight: 400;
+    font-style: 14px;
+`;
