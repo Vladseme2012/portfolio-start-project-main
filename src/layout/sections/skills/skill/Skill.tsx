6fe4bc475c97bc398a0c32dfc71ebd091e44650c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-14 19:50:45 +0300

    fixed component Skill

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -38,7 +38,7 @@ export const IconWrapper = styled.div`
         display: inline-block;
         width: 80px;
         height: 80px;
-        background-color: rgba(255, 255, 255, 0.1);
+        background-color: #ffffff19;
         transform: rotate(45deg) translate(-50%, -50%);
         content: '';
     }
