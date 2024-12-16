111a7f09d1055269c852f5b7fdab100fb04b55bb
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-16 21:36:12 +0300

    fixed HeaderMenu

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -29,6 +29,10 @@ const StyledHeaderMenu = styled.nav`
         display: flex;
         gap: 30px;
     }
+
+    @media ${theme.media.tablet} {
+        display: none;
+    }
 `;
 
 const Link = styled.a`
