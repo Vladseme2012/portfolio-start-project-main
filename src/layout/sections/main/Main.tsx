66fc2651af519061c6e1aee55817119558289f51
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-18 20:20:40 +0300

    fixed component Main for mobile

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import photo from './../../../assets/images/photo.jpg';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
 import { theme } from '../../../styles/Theme';
+import { font } from '../../../styles/Common';
 
 export const Main = () => {
     return (
@@ -41,19 +42,27 @@ const WelcomeText = styled.span`
 `;
 
 const MainTitle = styled.h1`
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 700;
-    font-style: 50px;
-    letter-spacing: 0.5em;
+    ${font({
+        weight: 400,
+        fontMax: 27,
+        fontMin: 20,
+    })}
 `;
 
 const Name = styled.h2`
-    font-weight: 400;
-    font-style: 27px;
+    ${font({
+        family: "'Josefin Sans', sans-serif",
+        weight: 700,
+        fontMax: 50,
+        fontMin: 36,
+    })}
+    letter-spacing: 0.5em;
     margin: 10px 0;
+
     span {
         position: relative;
         z-index: 1;
+        white-space: nowrap;
 
         &::before {
             position: absolute;
@@ -66,11 +75,17 @@ const Name = styled.h2`
             z-index: -1;
         }
     }
+
+    @media ${theme.media.mobile} {
+        margin: 15px 0 20px;
+    }
 `;
 
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
+    margin-top: 65px;
+
     &::before {
         position: absolute;
         top: -24px;
@@ -80,10 +95,12 @@ const PhotoWrapper = styled.div`
         height: 470px;
         border: 5px solid ${theme.colors.accent};
         z-index: -1;
-        
+
         @media ${theme.media.mobile} {
             width: 320px;
             height: 400px;
+            top: -17px;
+            left: 20px;
         }
     }
 `;
@@ -92,6 +109,7 @@ const Photo = styled.img`
     width: 350px;
     height: 450px;
     object-fit: cover;
+    margin-right: 15px;
 
     @media (${theme.media.mobile}) {
         width: 310px;
