190381254a0b66d6b9629245040ae13330159e9e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-18 19:22:30 +0300

    complited function for flexible fonts

--- src/styles/Common.tsx
+++ src/styles/Common.tsx
@@ -5,11 +5,14 @@ type FontPropsType = {
     weight?: number;
     color?: string;
     lineHeight?: number;
+    fontMin: number;
+    fontMax: number;
 };
 
-export const font = ({ family, weight, color, lineHeight }: FontPropsType) => `
+export const font = ({ family, weight, color, lineHeight, fontMin, fontMax }: FontPropsType) => `
   font-family: ${family || 'Poppins'};
   font-weight: ${weight || 400};
   color: ${color || theme.colors.colorText};
   line-height: ${lineHeight || 1.2};
+  font-size: calc( (100vw - 360)/(1440 - 360) * (${fontMax} - ${fontMin}) + ${fontMin}px);
 `;
