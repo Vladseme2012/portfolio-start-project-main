b92d7d7b9b2eb0e59b3ab22c5390ffca322b4c30
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 15:47:56 +0300

    added file Skills_Styles.tsx  with styles for components Skills and Skill

---
+++ src/layout/sections/skills/Skills_Styles.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { theme } from '../../../styles/Theme';
+
+// skills
+const SectionSkills = styled.section``;
+
+// skill
+const SkillArticle = styled.article`
+    max-width: 330px;
+    padding: 42px 20px 50px;
+    flex-grow: 1;
+
+    @media (${theme.media.mobile}) {
+        padding: 62px 0 40px;
+    }
+`;
+
+export const IconWrapper = styled.div`
+    position: relative;
+    &::before {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform-origin: top left;
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+        background-color: #ffffff19;
+        transform: rotate(45deg) translate(-50%, -50%);
+        content: '';
+    }
+`;
+
+const SkillTitle = styled.h3`
+    margin: 70px 0 15px;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-style: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+`;
+
+const SkillDescription = styled.p`
+    text-align: center;
+`;
+
+export const S = {
+    SectionSkills,
+    SkillArticle,
+    IconWrapper,
+    SkillTitle,
+    SkillDescription,
+};
