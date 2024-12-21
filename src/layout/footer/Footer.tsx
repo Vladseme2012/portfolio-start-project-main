cdf4a6cfebbaabee983316503994bb521bca7a37
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 21:42:53 +0300

    refactorind component Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,109 +1,48 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Icon } from '../../components/icon/Icon';
-import { theme } from '../../styles/Theme';
-import { font } from '../../styles/Common';
+import { S } from './Footer_Styles';
 
-export const Footer = () => {
-    return (
-        <StyledFooter>
-            <SocialName>Svetlana</SocialName>
-            <SocialList>
-                <SocialItem>
-                    <SocialIconLink>
-                        I
-                        <Icon
-                            iconId={'instagram'}
-                            width={'21px'}
-                            height={'21px'}
-                            viewBox={'0 0 21px 21px'}
-                        />
-                    </SocialIconLink>
-                </SocialItem>
-                <SocialItem>
-                    <SocialIconLink>
-                        I
-                        <Icon
-                            iconId={'telegram'}
-                            width={'21px'}
-                            height={'21px'}
-                            viewBox={'0 0 21px 21px'}
-                        />
-                    </SocialIconLink>
-                </SocialItem>
-                <SocialItem>
-                    <SocialIconLink>
-                        I
-                        <Icon
-                            iconId={'vk'}
-                            width={'21px'}
-                            height={'21px'}
-                            viewBox={'0 0 21px 21px'}
-                        />
-                    </SocialIconLink>
-                </SocialItem>
-                <SocialItem>
-                    <SocialIconLink>
-                        I
-                        <Icon
-                            iconId={'group'}
-                            width={'21px'}
-                            height={'21px'}
-                            viewBox={'0 0 21px 21px'}
-                        />
-                    </SocialIconLink>
-                </SocialItem>
-            </SocialList>
-            <SocialCopyright>
-                © 2023 Svetlana Dyablo, All Rights Reserved.
-            </SocialCopyright>
-        </StyledFooter>
-    );
+type SocialItemsData = {
+    iconId: string;
 };
 
-const StyledFooter = styled.footer`
-    padding: 40px 0;
-    background-color: ${theme.colors.primaryBg};
-`;
+const socialItemsData: Array<SocialItemsData> = [
+    {
+        iconId: 'instagram',
+    },
+    {
+        iconId: 'telegram',
+    },
+    {
+        iconId: 'vk',
+    },
+    {
+        iconId: 'group',
+    },
+];
 
-const SocialName = styled.span`
-    ${font({
-        family: "'Josefin Sans', sans-serif",
-        weight: 700,
-        fontMin: 16,
-        fontMax: 22,
-    })}
-
-    letter-spacing: 3px;
-`;
-
-const SocialList = styled.ul`
-    display: flex;
-    gap: 20px;
-    margin: 30px 0;
-`;
-const SocialItem = styled.li``;
-
-const SocialIconLink = styled.a`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-color: #ffffff19;
-    border-radius: 50%;
-    width: 35px;
-    height: 35px;
-
-    color: ${theme.colors.accent};
-
-    &:hover {
-        color: ${theme.colors.primaryBg};
-        transform: translateY(-4px);
-    }
-`;
-
-const SocialCopyright = styled.small`
-    font-weight: 400;
-    font-size: 12px;
-    text-align: center;
-    opacity: 0.5;
-`;
+export const Footer: React.FC = () => {
+    return (
+        <S.Footer>
+            <S.SocialName>Svetlana</S.SocialName>
+            <S.SocialList>
+                {socialItemsData.map((socialItem, index) => (
+                    <S.SocialItem key={index}>
+                        <S.SocialIconLink>
+                            I
+                            <Icon
+                                iconId={socialItem.iconId}
+                                width={'21px'}
+                                height={'21px'}
+                                viewBox={'0 0 21px 21px'}
+                            />
+                        </S.SocialIconLink>
+                    </S.SocialItem>
+                ))}
+            </S.SocialList>
+            <S.SocialCopyright>
+                © 2023 Svetlana Dyablo, All Rights Reserved.
+            </S.SocialCopyright>
+        </S.Footer>
+    );
+};
