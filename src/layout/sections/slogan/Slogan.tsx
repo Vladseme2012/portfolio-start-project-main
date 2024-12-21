6978342f2efbcbb57e24042c1747f551d118bc94
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 20:56:32 +0300

    refactoring component Slogan

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Container } from '../../../components/Container';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/Button';
+import { S } from './Slogan_Styles';
 
-export const Slogan = () => {
+export const Slogan: React.FC = () => {
     return (
-        <StyledSlogan>
+        <S.Slogan>
             <Container>
                 <FlexWrapper direction={'column'} align={'center'}>
                     <SectionTitle>I Am Available For Freelance</SectionTitle>
                     <Button>Hire me</Button>
                 </FlexWrapper>
             </Container>
-        </StyledSlogan>
+        </S.Slogan>
     );
 };
-
-const StyledSlogan = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    max-width: 540px;
-    min-height: 200px;
-`;
