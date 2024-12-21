01b8594da74d68dc0d801f3289130f959e5e686f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 20:56:02 +0300

    created files Slogan_Styles.tsx includes styles for Slogan

---
+++ src/layout/sections/slogan/Slogan_Styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Slogan = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 540px;
+    min-height: 200px;
+`;
+
+export const S = {
+    Slogan,
+};
