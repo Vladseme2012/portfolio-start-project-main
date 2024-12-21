531c94a393fb32ee9bfc1d56eec17a547549eb0a
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 20:09:01 +0300

    refactoring component TabMenu

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from '../../../../components/Link';
+import { S } from '../Work_Styles';
 
-export const TabMenu = (props: { items: Array<string> }) => {
+export const TabMenu:React.FC<{items: Array<string>}> = (props) => {
     return (
-        <StyledTabMenu>
+        <S.TabMenu>
             <ul>
                 {props.items.map((item, index) => (
-                    <ListItem key={item[index]}>
+                    <S.ListItem key={index}>
                         <Link href="">{item}</Link>
-                    </ListItem>
+                    </S.ListItem>
                 ))}
             </ul>
-        </StyledTabMenu>
+        </S.TabMenu>
     );
 };
-
-const StyledTabMenu = styled.nav`
-    ul {
-        max-width: 352px;
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 0 auto 40px;
-    }
-`;
-
-const ListItem = styled.li``;
