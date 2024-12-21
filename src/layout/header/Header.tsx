ce09c48e0b5bae3b303e2932c1ed5bc72b7baa43
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 14:04:34 +0300

    refactored Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
-import { HeaderMenu } from './headerMenu/HeaderMenu';
+import { DesktopMenu } from './headerMenu/DesktopMenu/DesktopMenu';
 import { Container } from '../../components/Container';
 import { FlexWrapper } from '../../components/FlexWrapper';
-import { theme } from '../../styles/Theme';
-import { MobileMenu } from './mobileMenu/MobileMenu';
+import { MobileMenu } from './headerMenu/mobileMenu/MobileMenu';
+import { S } from './Header_Styles';
+
 
 const itemsMenu = ['Home', 'Skills', 'Works', 'Testimony', 'Contact'];
 
-export const Header:React.FC = () => {
+export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 768;
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
+
     return (
-        <StyledHeader>
+        <S.Header>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo />
-                    <HeaderMenu items={itemsMenu} />
-                    <MobileMenu items={itemsMenu} />
+                    {width < breakpoint ? (
+                        <MobileMenu items={itemsMenu} />
+                    ) : (
+                        <DesktopMenu items={itemsMenu} />
+                    )}
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.Header>
     );
 };
-
-const StyledHeader = styled.header`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background-color: ${theme.colors.primaryBg};
-    padding: 20px 0;
-    z-index: 100;
-`;
