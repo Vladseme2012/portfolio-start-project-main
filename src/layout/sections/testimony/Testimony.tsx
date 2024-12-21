776bcb3306323078383c2bc6dad4cff7c22aa96e
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 21:05:57 +0300

    fixed component Testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -5,6 +5,7 @@ import { Slider } from '../../../components/slider/Slider';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
 import { St } from './Testimony_Styles';
+import { S } from '../skills/Skills_styles';
 
 export const Testimony: React.FC = () => {
     return (
