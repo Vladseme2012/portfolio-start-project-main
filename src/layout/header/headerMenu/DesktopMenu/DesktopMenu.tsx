56daf9d8b7c5c75e9ac848c62d0a6d1a5df4d2e2
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-21 20:19:48 +0300

    refactoring component DesktopMenu

--- src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
+++ src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Menu } from '../menu/Menu';
 import { S } from '../HeaderMenu_Styles';
 
-export const DesktopMenu: React.FC<{ items: Array<string> }> = (props: {
-    items: Array<string>;
-}) => {
+export const DesktopMenu: React.FC<{ items: Array<string> }> = (props) => {
     return (
         <S.DesktopMenu>
             <Menu items={props.items} />
