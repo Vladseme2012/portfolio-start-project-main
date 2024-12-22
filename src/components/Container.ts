89ced56aa6b383c0c1e75d5a831d2fe32022c582
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-22 10:32:46 +0300

    refactoring styled component Container

---
+++ src/components/Container.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    max-width: 1140px;
+    min-height: 100%;
+    margin: 0 auto;
+    padding: 15px;
+`;
