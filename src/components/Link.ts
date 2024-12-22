4b40abd6312dea55d4af58f8d82aaef378e7c444
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-22 10:34:20 +0300

    refactoring styled component Link

---
+++ src/components/Link.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { theme } from '../styles/Theme';
+
+export const Link = styled.a`
+    position: relative;
+    padding: 10px;
+    font-weight: 400;
+    font-style: 14px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    z-index: 0;
+    
+    &:hover {
+        &::before {
+            height: 10px;
+        }
+    }
+
+    &::before {
+        display: inline-block;
+        color: ${theme.colors.accent};
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 5px;
+        content: '';
+        z-index: -1;
+    }
+`;
