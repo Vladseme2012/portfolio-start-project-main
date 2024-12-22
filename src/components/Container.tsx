7fed1639fa3fab7c78b7eb797dac0c19586d948c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-22 10:37:08 +0300

    renamed extention for files Button.tsx, Container.tsx, FlexWrapper.tsx, Link.tsx, SectionTitle.tsx

--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    width: 100%;
-    max-width: 1140px;
-    min-height: 100%;
-    margin: 0 auto;
-    padding: 15px;
-`;
