be641e3d81c9cf794838363d7ff5af8db04bcf2c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-24 21:40:04 +0300

    fixed path theme

--- src/layout/header/headerMenu/menu/HeaderMenu_Styles.tsx
+++ src/layout/header/headerMenu/menu/HeaderMenu_Styles.tsx
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
-import { theme } from '../../../styles/Theme';
+import { theme } from '../../../../styles/Theme';
+
 
 // Menu
 const Link = styled.a`
