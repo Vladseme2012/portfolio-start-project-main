b390a164d8e41f025e8a8eb2446063093f845901
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-24 21:42:03 +0300

    fixed path to HeaderMenu_Styles

--- src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
+++ src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu } from '../menu/Menu';
-import { S } from '../HeaderMenu_Styles';
+import { S } from '../menu/HeaderMenu_Styles';
+
 
 export const DesktopMenu: React.FC<{ items: Array<string> }> = (props) => {
     return (
