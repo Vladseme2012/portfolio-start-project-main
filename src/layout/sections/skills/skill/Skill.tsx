53f71a080b8bfb841b817acdd3e3e19a36709300
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-25 17:55:32 +0300

    fixed path to ../Skills_Styles

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FlexWrapper } from '../../../../components/FlexWrapper';
 import { Icon } from '../../../../components/icon/Icon';
-import { S } from '../Skills_styles';
+import { S } from '../Skills_Styles';
 
 type SkillPropsType = {
     iconId: string;
