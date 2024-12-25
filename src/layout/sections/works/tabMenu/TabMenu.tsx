76a0d998203b30a425162b9bf277bbf0dafb4b38
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-25 11:54:03 +0300

    fixed type props for TabMenu

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from '../../../../components/Link';
 import { S } from '../Work_Styles';
 
-export const TabMenu: React.FC<{ tabsItems: Array<string> }> = (props) => {
+export const TabMenu: React.FC<{
+    tabsItems: Array<{
+        title: string;
+        status: 'all' | 'landing' | 'react' | 'spa';
+    }>;
+}> = (props) => {
     return (
         <S.TabMenu>
             <ul>
                 {props.tabsItems.map((tab, index) => (
                     <S.ListItem key={index}>
-                        <Link href="">{tab}</Link>
+                        <Link href="">{tab.title}</Link>
                     </S.ListItem>
                 ))}
             </ul>
