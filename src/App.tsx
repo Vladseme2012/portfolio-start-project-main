0984a0f93c93cc8b5416cac95ab7ee32f1ff5780
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-26 20:19:50 +0300

    added component Pacticle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css';
-import { Footer } from './layout/footer/Footer';
+import { Particle } from './components/particle/Particle';
 import { Header } from './layout/header/Header';
-import { Contact } from './layout/sections/contacts/Contact';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
-import { Slogan } from './layout/sections/slogan/Slogan';
-import { Testimony } from './layout/sections/testimony/Testimony';
 import { Works } from './layout/sections/works/Works';
+import { Testimony } from './layout/sections/testimony/Testimony';
+import { Contact } from './layout/sections/contacts/Contact';
+import { Slogan } from './layout/sections/slogan/Slogan';
+import { Footer } from './layout/footer/Footer';
 
 function App() {
     return (
         <div className="App">
+            <Particle />
             <Header />
             <Main />
             <Skills />
