04ee55c11a834e8d78d9524ab962132e80fec85f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-27 19:59:53 +0300

    fixed component DesktopMenu

--- src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
+++ src/layout/header/headerMenu/DesktopMenu/DesktopMenu.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Menu } from '../menu/Menu';
 import { S } from '../menu/HeaderMenu_Styles';
 
-
-export const DesktopMenu: React.FC<{ items: Array<string> }> = (props) => {
+export const DesktopMenu: React.FC = () => {
     return (
         <S.DesktopMenu>
-            <Menu items={props.items} />
+            <Menu />
         </S.DesktopMenu>
     );
 };
