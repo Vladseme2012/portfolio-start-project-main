49461b5d7f253d9780bd982dbe957efad35139ec
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-28 15:20:26 +0300

    conected form with emailjs service

--- src/layout/sections/contacts/Contact.tsx
+++ src/layout/sections/contacts/Contact.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { ElementRef, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/Button';
 import { Container } from '../../../components/Container';
 import { S } from './Contacts_Styles';
 
 export const Contact: React.FC = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        if (!form.current) {
+            return;
+        }
+
+        emailjs
+            .sendForm('service_xcimrg8', 'template_sggpor3', form.current, {
+                publicKey: '9m6lYRsFCWmWemzI9',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                }
+            );
+    };
     return (
         <S.Contacts id={'contact'}>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <S.Form action="">
-                    <S.Field placeholder={'name'} />
-                    <S.Field placeholder={'subject'} />
-                    <S.Field as={'textarea'} placeholder={'massage'} />
+                <S.Form ref={form} onSubmit={sendEmail}>
+                    <S.Field placeholder={'name'} name={'user_name'} />
+                    <S.Field placeholder={'subject'} name={'subject'} />
+                    <S.Field
+                        as={'textarea'}
+                        placeholder={'message'}
+                        name={'message'}
+                    />
                     <Button type={'submit'}>Send message</Button>
                 </S.Form>
             </Container>
